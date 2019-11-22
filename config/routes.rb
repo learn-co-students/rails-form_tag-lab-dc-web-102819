884da98b6b166d28d5431bbac583be343f0f172d
Kyle <kylepcole@gmail.com>
2019-11-22 08:48:32 -0500

    not working at all

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :students, only: :index
-  get '/students/:id', to: 'students#show', as: 'student'
+  resources :students#, only: [:index]
+  # get '/students', to: 'students#new'#, as: 'new_student_path'
+  # get '/students/:id', to: 'students#show', as: 'student'
+  # get '/students', to: 'students#index'
 end
