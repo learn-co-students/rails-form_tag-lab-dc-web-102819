a2cc0bb87f875197d408ab2fbcd8c866fbc98b08
github username <github email address>
2019-11-22 17:44:51 -0500

    working on form page

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,25 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
+  end
+
+
+  def new
+    # if Student.all.last
+  	# 	@student_firstname = Student.all.last.first_name
+  	# 	@student_lastname= Student.all.last.last_name
+  	# else
+  	# 	@student_firstname =''
+  	# 	@student_lastname = ''
+  	# end
+  end
+
+  def create
+    # first_name = params[:first_name]
+    # last_name = params[:last_name]
+    student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to new_student_path
   end
 
   def show
