edff03c6ed959bbcec55a3cb0b49362e4942abf6
jdromero88 <github email address>
2019-11-23 01:17:05 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,20 +6,14 @@ class StudentsController < ApplicationController
 
 
   def new
-    # if Student.all.last
-  	# 	@student_firstname = Student.all.last.first_name
-  	# 	@student_lastname= Student.all.last.last_name
-  	# else
-  	# 	@student_firstname =''
-  	# 	@student_lastname = ''
-  	# end
+  
   end
 
   def create
-    # first_name = params[:first_name]
-    # last_name = params[:last_name]
-
-    student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    first_name = params[:first_name]
+    last_name = params[:last_name]
+    student = Student.create(first_name: first_name, last_name: last_name)
+    # student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
 
     redirect_to new_student_path
   end
