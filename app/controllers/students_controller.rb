5863e761e16c679dd770a954bfbd22e20015a6d6
jdromero88 <github email address>
2019-11-29 12:25:31 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,16 +6,17 @@ class StudentsController < ApplicationController
 
 
   def new
-  
+
   end
 
   def create
-    first_name = params[:first_name]
-    last_name = params[:last_name]
-    student = Student.create(first_name: first_name, last_name: last_name)
+    # byebug
+    first_name = params[:student][:first_name]
+    last_name = params[:student][:last_name]
+    @student = Student.create(first_name: first_name, last_name: last_name)
     # student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
 
-    redirect_to new_student_path
+    redirect_to student_path(@student)
   end
 
   def show
